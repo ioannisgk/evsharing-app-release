7f682ffcbe80c39c1f500b550b5463dd8cf84127
ioannisgk <bc02118123>
2017-09-18 16:50:22 +0300

    Initial commit

---
+++ app/src/main/java/com/ioannisgk/evsharingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ioannisgk.evsharingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
